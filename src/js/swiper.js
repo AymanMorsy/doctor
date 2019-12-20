1e84b45ca695097eacffde3cc8267c24abd76abc
AymanMorsy <aymanmorsy.eg@gmail.com>
2019-12-20 10:00:41 +0200

    slider adjustment

---
+++ src/js/swiper.js
@@ -0,0 +1,26 @@
+import { Swiper, Navigation,Pagination,Parallax ,Autoplay,EffectCube} from 'swiper/js/swiper.esm.js';
+// Install modules
+Swiper.use([Navigation,Pagination,Parallax,Autoplay,EffectCube]);
+
+// Now you can use Swiper
+// لما تيجي تعمل سلايدر تاني هتعمله انستنس تانية بانشليز جديدة
+var swiper = new Swiper('.swiper-container', {
+pagination: {
+el: '.swiper-pagination',
+type: 'progressbar',
+},
+// autoplay: {
+//   delay: 3000,
+// },
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
+loop: true,
+});
+
+
+swiper.on('slideChange', function () {
+    // $('.swiper-slide h1').classList.remove("active")
+    $('.swiper-slide h1').classList.add("active")
+  });
