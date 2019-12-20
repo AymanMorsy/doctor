98a0217aadb99de5b94e64467a665573f217cea0
AymanMorsy <aymanmorsy.eg@gmail.com>
2019-12-20 10:00:08 +0200

    rmv slider script to seprate module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
-import { Swiper, Navigation,Pagination ,Autoplay,EffectCube} from 'swiper/js/swiper.esm.js';
 import './sass/vendors/swiper.scss';
 import './sass/main.scss';
 import  './js/news-ticker';
 import  './js/nav';
+import  './js/swiper';
 
-// Install modules
-Swiper.use([Navigation,Pagination,Autoplay,EffectCube]);
 
-// Now you can use Swiper
-var swiper = new Swiper('.swiper-container', {
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar',
-  },
-    autoplay: {
-      delay: 2500,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
 
 
 
